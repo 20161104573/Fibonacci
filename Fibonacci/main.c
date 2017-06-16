3089018c3b9792001e89ba3e1b2e2724b7489c5a
20161104573 <a20161104573@admindeiMac-34.local>
2017-06-16 15:09:30 +0800

    斐波那契数列
    斐波那契数列

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -8,8 +8,21 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int f1,f2,f3,i,n,j,a;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        f1=f2=f3=1;
+        scanf("%d",&a);
+        for(j=3;j<=a;j++)
+        {
+            f3=f1+f2;
+            f1=f2;
+            f2=f3;
+        }
+        printf("%d\n",f3);
+    }
     return 0;
 }
